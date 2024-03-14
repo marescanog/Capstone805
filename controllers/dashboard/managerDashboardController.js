e2ac5819309fb9bd028f28a2a4a0abf4882667be
marescanog <marescanog@gmail.com>
2024-03-13 20:06:50 -0400

    completed side acc info partial for dashbaord

---
+++ controllers/dashboard/managerDashboardController.js
@@ -0,0 +1,7 @@
+
+exports.loadManagerDashboard = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The loadStaffDashboard route is not yet defined!'
+    });
+}
---
+++ controllers/dashboard/staffDashboardController.js
@@ -0,0 +1,71 @@
+
+exports.loadStaffDashboard = async (req, res) => {
+      res.render( "pages/employee/empDashboard",{ 
+        layout:"main", 
+        css: 'dash.css', 
+        title:'Employee Dashboard',
+        partialsCSS: [
+            // {name:"h1styled.css"},
+            {name:"accountInfoSideBar.css"}
+        ],
+        sidebarData : {
+            img: "/img/placeholder/hotelstaff.png",
+            firstName: "Shawna",
+            lastName: "M.",
+            employeeType: "Hotel Staff",
+            mobileNumber: "499-999-999",
+            address: "address, city, postal code, country",
+            emailAddress: "shawnam@gmail.com"
+        }
+    }); 
+}
+
+exports.viewStaffReservations = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewStaffReservations route is not yet defined!'
+    });
+}
+
+exports.editStaffAccount = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The editStaffAccount route is not yet defined!'
+    });
+}
+
+exports.editStaffPassword = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The editStaffPassword route is not yet defined!'
+    });
+}
+
+exports.createReservations = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The createReservations route is not yet defined!'
+    });
+}
+
+exports.viewInquiries = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewInquiries route is not yet defined!'
+    });
+}
+
+exports.checkin = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The checkin route is not yet defined!'
+    });
+}
+
+exports.updateStaffPhoto = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The updateStaffPhoto route is not yet defined!'
+    });
+}
+
