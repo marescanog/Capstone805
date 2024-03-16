177ee938b692b3e65639f9fc7e5a1533806ff678
marescanog <marescanog@gmail.com>
2024-03-15 20:33:38 -0400

    added breadcrumbs partial

---
+++ public/js/breadcrumbs.js
@@ -0,0 +1,21 @@
+// refactor to own css file
+function updateProgress(stage) {
+    // Reset all to default state
+    document.querySelectorAll('.circle, .progress').forEach(el => {
+      el.classList.remove('highlighted');
+    });
+    // Highlight based on current stage
+    if (stage >= 1) {
+      document.querySelector('#start .circle').classList.add('highlighted');
+    }
+    if (stage >= 2) {
+      document.querySelector('#middle .circle').classList.add('highlighted');
+      document.querySelectorAll('.progress')[0].classList.add('highlighted');
+      document.querySelector('#start .circle img').src = "/img/icons/general/check.svg";
+    }
+    if (stage === 3) {
+      document.querySelector('#end .circle').classList.add('highlighted');
+      document.querySelectorAll('.progress')[1].classList.add('highlighted'); 
+      document.querySelector('#middle .circle img').src = "/img/icons/general/check.svg";
+    }
+  }
