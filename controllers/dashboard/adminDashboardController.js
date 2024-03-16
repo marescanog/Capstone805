f6434edf5cd13479bec76d556fb8f96338f52490
marescanog <marescanog@gmail.com>
2024-03-16 18:12:50 -0400

    added routes

--- controllers/dashboard/adminDashboardController.js
+++ controllers/dashboard/adminDashboardController.js
@@ -5,3 +5,10 @@ exports.loadAdminDashboard = async (req, res) => {
         message: 'The loadAdminDashboard route is not yet defined!'
     });
 }
+
+exports.viewUsers = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewUsers route is not yet defined!'
+    });
+}
--- controllers/dashboard/managerDashboardController.js
+++ controllers/dashboard/managerDashboardController.js
@@ -2,6 +2,27 @@
 exports.loadManagerDashboard = async (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'The loadStaffDashboard route is not yet defined!'
+        message: 'The loadManagerDashboard route is not yet defined!'
+    });
+}
+
+exports.viewOffers = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewOffers route is not yet defined!'
+    });
+}
+
+exports.viewPromotions = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewPromotions route is not yet defined!'
+    });
+}
+
+exports.viewRooms = async (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'The viewRooms route is not yet defined!'
     });
 }
