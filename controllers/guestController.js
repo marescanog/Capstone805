af50edf5bb801eb6ee35d33e9671d793567dea77
Sanam Maharjan <sanammaharjan@Sanams-MacBook-Pro.local>
2024-03-18 16:31:17 -0400

    changed

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -10,8 +10,38 @@ exports.getAllGuests = async (req, res) => {
 }
 
 exports.getGuestById = async (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'The getGuestById route is not yet defined!'
-    });
+    try {
+        const { id } = req.params;
+
+        // Check if the provided ID is a valid ObjectId
+        if (!Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid guest ID'
+            });
+        }
+
+        // Query the database to fetch the guest user by ID
+        const guest = await Guest.findById(id);
+
+        if (!guest) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'Guest user not found'
+            });
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                guest
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal server error'
+        });
+    }
 }
