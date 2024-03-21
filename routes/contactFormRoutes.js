4b80d066ada20aa22d68133b94a0dbe0ce68534e
marescanog <marescanog@gmail.com>
2024-03-21 04:48:57 -0400

    added route

--- routes/contactFormRoutes.js
+++ routes/contactFormRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 var app = express();
-const {getAllUnregisteredContactForms, getUnregisteredContactFormsById} = require('./../controllers/contactFormController.js');
+const {getAllUnregisteredContactForms, getUnregisteredContactFormsById, submitContactForm} = require('./../controllers/contactFormController.js');
 const contactFormRouter = express.Router();
 
 contactFormRouter
   .route('/')
-  .get(getAllUnregisteredContactForms);
-
+  .get(getAllUnregisteredContactForms)
+  
   contactFormRouter
   .route('/:id')
   .get(getUnregisteredContactFormsById);
+
+  contactFormRouter
+  .route('/submit')
+  .get(submitContactForm)
 
 module.exports = contactFormRouter;
