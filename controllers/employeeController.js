8ebf0c5aac9ad630e6bb006f3b9e6a4167ab7ff0
nischal sapkota <nischalsapkot977@gmail.com>
2024-03-25 18:46:10 -0400

    updated employee api

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -2,16 +2,46 @@ const mongoose = require('mongoose');
 const Employee = require('../models/employeeModel.js');
 const {Types} = mongoose;
 
+
 exports.getAllEmployees = async (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'The getAllEmployees route is not yet defined!'
-    });
-}
+    try {
+        const employees = await Employee.find();
+        res.status(200).json({
+            status: 'success',
+            data: {
+                employees
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: 'error',
+            message: err.message
+        });
+    }
+};
 
 exports.getEmployeeById = async (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'The getEmployeeById route is not yet defined!'
-    });
-}
+    try {
+        const employee = await Employee.findById({ _id: req.params.id });
+
+        if (!employee) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'Employee not found'
+            });
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                employee
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            status: 'error',
+            message: err.message
+        });
+    }
+};
