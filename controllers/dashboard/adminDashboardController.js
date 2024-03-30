17771d8f547e85f8b47a44445994a5cd1c5b9cf3
marescanog <marescanog@gmail.com>
2024-03-30 10:11:23 -0400

    added admin pages routes and linking

--- controllers/dashboard/adminDashboardController.js
+++ controllers/dashboard/adminDashboardController.js
@@ -31,8 +31,8 @@ exports.loadAdminDashboard = catchAsync(async (req, res, next) => {
         });
         VB.addOptions("buttonData", [
             {name:"Manage Guests",url:"/dashboard/USNVMQD493/users"},
-            {name:"Manage Employees",url:"#"},
-            {name:"Manage Permissions",url:"#"}
+            {name:"Manage Employees",url:"/dashboard/USNVMQD493/employees"},
+            {name:"Manage Permissions",url:"/dashboard/USNVMQD493/managepermissions"}
         ]);
         VB.addOptions("scripts",[
             {src:"/js/managerDash.js"},
@@ -44,26 +44,63 @@ exports.loadAdminDashboard = catchAsync(async (req, res, next) => {
 } )
 
 exports.viewUsers = catchAsync(async (req, res, next) => {
-    res.render( "pages/employee/viewList",{ 
-        layout:"main", 
-        css: 'employee/checkin.css', 
-        title:'View Guests',
-        partialsCSS: [
+    if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/checkin.css");
+        VB.addOptions("title", "View Guests");
+        VB.addOptions("headerTitle", "Find User Accounts");
+        VB.addOptions("placeholder", "Search for a User");
+        VB.addOptions("partialsCSS", [
             {name:"h1styled.css"},
             {name:"search.css"},
             {name:"table.css"},
-        ],
-        scripts: [
+        ]);
+        VB.addOptions("scripts", [
             {src:"/js/searchControl.js"},
-        ],
-        searchOptionsList: [
+            {src:"/js/tableButtonFunc/viewUsersTableButtons.js"},
+        ]);
+        VB.addOptions("searchOptionsList", [
             {id:"accountID", label:"Account ID"},
             {id:"lastName", label:"Last Name"},
             {id:"firstName", label: "First Name"},
             {id:"createdOn", label: "Created On"}
-        ],
-        searchControlUrl: "/dashboard/USNVMQD493/users",
-        results: [
+        ]);
+        VB.addOptions("searchControlUrl", "/dashboard/USNVMQD493/users");
+    
+        VB.addOptions("tableOptions", {
+            columns: [
+                {resultName:"accountID", label:"Accound I.D.", isRow: true},
+                {resultName:"createdOn", label:"Created On"},
+                {resultName:"firstName", label:"First Name"},
+                {resultName:"lastName", label:"Last Name"},
+                {resultName:"status", label:"Status"},
+                {resultName:"isVerified", label:"Verified"},
+                {resultName:"action1", label:"Action", 
+                    isButton: { 
+                        classNames: [{classname:"btn-tab-lightblue", name: "View/Edit"}],
+                        buttonActions: [{classname:'Redirect', name: "View/Edit"}],
+                    }
+                },
+                {resultName:"action2", label:"Action", 
+                    isButton: { 
+                        classNames: [{classname:"btn-warning", name: "Permissions"}],
+                        buttonActions: [{classname:'Permission', name: "Permissions"}],
+                    }
+                },
+                ,
+                {resultName:"action3", label:"Action", 
+                    isButton: { 
+                        classNames: [{classname:"btn-danger", name: "Delete"}],
+                        buttonActions: [{classname:'Delete', name: "Delete"}],
+                    }
+                }
+            ]
+        });
+        VB.addOptions("results", [
             {
                 accountID: "SDF4HN",
                 createdOn: "March 16, 2024 Fri",
@@ -71,10 +108,10 @@ exports.viewUsers = catchAsync(async (req, res, next) => {
                 lastName: "Lee",
                 isVerified: "true",
                 status: "active",
-                userType: "Guest",
-                action1: "Edit",
+                action1: "View/Edit",
                 action2: "Permissions",
-                action3: "Delete"
+                action3: "Delete",
+                variableName: "accountID"
             },
             {
                 accountID: "DSADASDS",
@@ -83,40 +120,187 @@ exports.viewUsers = catchAsync(async (req, res, next) => {
                 lastName: "Marsano",
                 isVerified: "true",
                 status: "active",
-                userType: "Manager",
-                action1: "Edit",
+                action1: "View/Edit",
                 action2: "Permissions",
-                action3: "Delete"
+                action3: "Delete",
+                variableName: "accountID"
             }
-        ],
-        tableOptions: {
+        ]);
+        res.render( "pages/employee/viewList", VB.getOptions());
+    } else {
+        return next(new AppError('You are not logged in! Please login to get access.', 401));
+    }
+    
+});
+
+
+exports.viewEmployees = catchAsync(async (req, res, next) => {
+    if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/checkin.css");
+        VB.addOptions("title", "View Employees");
+        VB.addOptions("headerTitle", "Find Employee Accounts");
+        VB.addOptions("placeholder", "Search for an Employee");
+        VB.addOptions("partialsCSS", [
+            {name:"h1styled.css"},
+            {name:"search.css"},
+            {name:"table.css"},
+        ]);
+        VB.addOptions("scripts", [
+            {src:"/js/searchControl.js"},
+            {src:"/js/tableButtonFunc/viewEmployeesTableButtons.js"},
+        ]);
+        VB.addOptions("searchOptionsList", [
+            {id:"accountID", label:"Account ID"},
+            {id:"lastName", label:"Last Name"},
+            {id:"firstName", label: "First Name"},
+            {id:"hiredDate", label: "Hire Date"}
+        ]);
+        VB.addOptions("searchControlUrl", "/dashboard/USNVMQD493/users");
+    
+        VB.addOptions("tableOptions", {
             columns: [
                 {resultName:"accountID", label:"Accound I.D.", isRow: true},
-                {resultName:"createdOn", label:"Created On"},
+                {resultName:"hiredDate", label:"Hire Date"},
                 {resultName:"firstName", label:"First Name"},
                 {resultName:"lastName", label:"Last Name"},
                 {resultName:"status", label:"Status"},
-                {resultName:"isVerified", label:"Verified"},
                 {resultName:"userType", label:"User Type"},
                 {resultName:"action1", label:"Action", 
                     isButton: { 
-                        classNames: [{classname:"btn-tab-lightblue", name: "Edit"}],
+                        classNames: [{classname:"btn-tab-lightblue", name: "Edit/View"}],
+                        buttonActions: [{classname:'Redirect', name: "Edit/View"}],
                     }
                 },
                 {resultName:"action2", label:"Action", 
                     isButton: { 
-                        classNames: [{classname:"btn-tab-lightblue", name: "Permissions"}],
+                        classNames: [{classname:"btn-warning", name: "Permissions"}],
+                        buttonActions: [{classname:'Permission', name: "Permissions"}],
                     }
                 },
                 ,
                 {resultName:"action3", label:"Action", 
                     isButton: { 
-                        classNames: [{classname:"btn-tab-lightblue", name: "Delete"}],
+                        classNames: [{classname:"btn-danger", name: "Delete"}],
+                        buttonActions: [{classname:'Delete', name: "Delete"}],
                     }
                 }
             ]
-        },
-        headerTitle:"Find User Accounts",
-        placeholder:"Search for a User"
-    }); 
-})
+        });
+        VB.addOptions("results", [
+            {
+                accountID: "SDF4HN",
+                hiredDate: "March 16, 2024 Fri",
+                firstName: "Gary",
+                lastName: "Lee",
+                status: "active",
+                userType: "Staff",
+                action1: "Edit/View",
+                action2: "Permissions",
+                action3: "Delete",
+                variableName: "accountID"
+            },
+            {
+                accountID: "DSADASDS",
+                hiredDate: "March 16, 2024 Fri",
+                firstName: "Dean",
+                lastName: "Marsano",
+                status: "active",
+                userType: "Manager",
+                action1: "Edit/View",
+                action2: "Permissions",
+                action3: "Delete",
+                variableName: "accountID"
+            }
+        ]);
+        res.render( "pages/employee/viewList", VB.getOptions());
+    } else {
+        return next(new AppError('You are not logged in! Please login to get access.', 401));
+    }
+});
+
+exports.createUserPage = catchAsync(async (req, res, next) => {
+    // if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/createforms.css");
+        VB.addOptions("title", "Create Guest Account");
+        VB.addOptions("headerTitle", "Create Guest Account");
+        res.render( "pages/employee/admin/createUser", VB.getOptions());
+    // }else {
+    //     return next(new AppError('You are not logged in! Please login to get access.', 401));
+    // }
+});
+
+exports.viewUserPage = catchAsync(async (req, res, next) => {
+    // if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/createforms.css");
+        VB.addOptions("readOnly", true);
+        VB.addOptions("title", "View Guest Account");
+        VB.addOptions("headerTitle", "View Guest Account");
+        res.render( "pages/employee/admin/createUser", VB.getOptions());
+    // }else {
+    //     return next(new AppError('You are not logged in! Please login to get access.', 401));
+    // }
+});
+
+exports.createEmployeePage = catchAsync(async (req, res, next) => {
+    // if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/createforms.css");
+        VB.addOptions("title", "Create Employee Account");
+        VB.addOptions("headerTitle", "Create Employee Account");
+        res.render( "pages/employee/admin/createEmployee", VB.getOptions());
+    // }else {
+    //     return next(new AppError('You are not logged in! Please login to get access.', 401));
+    // }
+});
+
+exports.viewEmployeePage = catchAsync(async (req, res, next) => {
+    // if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("readOnly", true);
+        VB.addOptions("css", "employee/createforms.css");
+        VB.addOptions("title", "View Employee Account");
+        VB.addOptions("headerTitle", "View Employee Account");
+        res.render( "pages/employee/admin/createEmployee", VB.getOptions());
+    // }else {
+    //     return next(new AppError('You are not logged in! Please login to get access.', 401));
+    // }
+});
+
+exports.managePermissionsPage = catchAsync(async (req, res, next) => {
+    // if(req.user){
+        const VB = new ViewBuilder({
+            alertToLogin: req?.alertToLogin??false,
+            userType: req?.decoded?.type??null,
+            id:req?.decoded?.id??null,
+        });
+        VB.addOptions("css", "employee/createforms.css");
+        VB.addOptions("title", "Manage Permissions");
+        VB.addOptions("headerTitle", "Manage Permissions");
+        res.render( "pages/employee/admin/manageUser", VB.getOptions());
+    // }else {
+    //     return next(new AppError('You are not logged in! Please login to get access.', 401));
+    // }
+});
